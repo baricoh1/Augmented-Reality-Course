5b97de9bef2ab9e42fef8a11cc1dee20cf368301
baricoh1 <baricoh1@gmail.com>
2025-11-26 18:01:53 +0200

    Update CubeRotate.cs

--- Assets/Scripts/CubeRotate.cs
+++ Assets/Scripts/CubeRotate.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;  
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CubeRotate : MonoBehaviour
 {
@@ -10,25 +11,28 @@ public class CubeRotate : MonoBehaviour
         // סיבוב קבוע
         transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
 
-        Vector3 movement = Vector3.zero;
+        if (Keyboard.current != null)
+        {
+            Vector3 movement = Vector3.zero;
 
-        // W או חץ למעלה
-        if (Input.GetKey(KeyCode.W)  Input.GetKey(KeyCode.UpArrow))
-            movement.z = 1;
+            // W או חץ למעלה
+            if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+                movement.z = 1;
 
-        // S או חץ למטה
-        if (Input.GetKey(KeyCode.S)  Input.GetKey(KeyCode.DownArrow))
-            movement.z = -1;
+            // S או חץ למטה
+            if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+                movement.z = -1;
 
-        // A או חץ שמאלה
-        if (Input.GetKey(KeyCode.A)  Input.GetKey(KeyCode.LeftArrow))
-            movement.x = -1;
+            // A או חץ שמאלה
+            if (Keyboard.current.aKey.isPressed || Keyboard.current.leftArrowKey.isPressed)
+                movement.x = -1;
 
-        // D או חץ ימינה
-        if (Input.GetKey(KeyCode.D)  Input.GetKey(KeyCode.RightArrow))
-            movement.x = 1;
+            // D או חץ ימינה
+            if (Keyboard.current.dKey.isPressed || Keyboard.current.rightArrowKey.isPressed)
+                movement.x = 1;
 
-        // הזזה בפועל
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+            // הזזה בפועל
+            transform.Translate(movement * moveSpeed * Time.deltaTime);
+        }
     }
 }
