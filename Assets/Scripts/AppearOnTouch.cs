b68e43ed8031d2d5e87db1aa8f10eb08b4b83ed4
baricoh1 <baricoh1@gmail.com>
2025-11-28 19:19:14 +0200

    גג

--- Assets/Scripts/AppearOnTouch.cs
+++ Assets/Scripts/AppearOnTouch.cs
@@ -1,39 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation; // חובה בשביל AR
+using UnityEngine.XR.ARSubsystems; // חובה בשביל סוגי פגיעה
+using System.Collections.Generic;    // בשביל רשימות
 
 public class AppearOnTouch : MonoBehaviour
 {
-    public GameObject legoModel; // לכאן נגרור את המודל
-    public float distance = 0.5f; // מרחק חצי מטר מהמצלמה
+    public GameObject legoModel;      // המודל למיקום
+    public ARRaycastManager raycastManager; // המנהל שאחראי על זיהוי הפגיעה במשטח
+
+    // רשימה שתשמור את המידע על איפה הקרן פגעה
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     void Update()
     {
-        // בדיקה אם יש לחיצה על המסך
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        // בדיקת לחיצה בטלפון
+        if (Input.touchCount > 0)
         {
-            SpawnLego();
-        }
+            Touch touch = Input.GetTouch(0);
 
-        // בדיקה למחשב (קליק עכבר) - כדי שתוכל לבדוק בלי לבנות לטלפון
-        if (Input.GetMouseButtonDown(0))
-        {
-            SpawnLego();
+            // בודקים רק ברגע הנגיעה הראשונית
+            if (touch.phase == TouchPhase.Began)
+            {
+                // שליחת קרן: (נקודת המגע במסך, הרשימה לשמירת התוצאות, וסוג המשטח)
+                // TrackableType.PlaneWithinPolygon מבטיח שזה יפגע רק בתוך הגבולות של המישור שזוהה
+                if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    // הפגיעה הראשונה (hits[0]) היא הקרובה ביותר למצלמה
+                    Pose hitPose = hits[0].pose;
+
+                    SpawnLego(hitPose.position);
+                }
+            }
         }
     }
 
-    void SpawnLego()
+    void SpawnLego(Vector3 positionOnPlane)
     {
-        // 1. הזזת המודל שיהיה מול המצלמה
-        legoModel.transform.position = Camera.main.transform.position + (Camera.main.transform.forward * distance);
+        // 1. הזזת המודל למיקום הפגיעה במישור
+        legoModel.transform.position = positionOnPlane;
 
-        // 2. סיבוב המודל שיהיה מול המשתמש (אופציונלי, כדי שלא יראה עקום)
+        // 2. סיבוב המודל שיהיה מול המשתמש (אבל יישאר ישר על הרצפה)
+        // אנחנו לוקחים את הכיוון למצלמה, אבל מאפסים את הגובה (y) כדי שהמודל לא ייטה למעלה/למטה
         Vector3 lookPos = Camera.main.transform.position - legoModel.transform.position;
         lookPos.y = 0;
         legoModel.transform.rotation = Quaternion.LookRotation(-lookPos);
 
-        // 3. הפעלת המודל (זה אוטומטית יפעיל את האנימציה מהתחלה!)
+        // 3. הפעלת המודל
         legoModel.SetActive(true);
 
-        // אופציונלי: מכבים את הסקריפט הזה כדי שלא יזמן את הלגו שוב ושוב בכל לחיצה
-        this.enabled = false;
+        // אופציונלי: מכבים את הסקריפט כדי שלא יזוז יותר
+        // this.enabled = false; 
     }
 }
