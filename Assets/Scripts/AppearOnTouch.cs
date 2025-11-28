ae37b7817021b290fe016e51c3cc1a809bea6568
baricoh1 <baricoh1@gmail.com>
2025-11-28 19:10:36 +0200

    דד

---
+++ Assets/Scripts/AppearOnTouch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class AppearOnTouch : MonoBehaviour
+{
+    public GameObject legoModel; // לכאן נגרור את המודל
+    public float distance = 0.5f; // מרחק חצי מטר מהמצלמה
+
+    void Update()
+    {
+        // בדיקה אם יש לחיצה על המסך
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            SpawnLego();
+        }
+
+        // בדיקה למחשב (קליק עכבר) - כדי שתוכל לבדוק בלי לבנות לטלפון
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnLego();
+        }
+    }
+
+    void SpawnLego()
+    {
+        // 1. הזזת המודל שיהיה מול המצלמה
+        legoModel.transform.position = Camera.main.transform.position + (Camera.main.transform.forward * distance);
+
+        // 2. סיבוב המודל שיהיה מול המשתמש (אופציונלי, כדי שלא יראה עקום)
+        Vector3 lookPos = Camera.main.transform.position - legoModel.transform.position;
+        lookPos.y = 0;
+        legoModel.transform.rotation = Quaternion.LookRotation(-lookPos);
+
+        // 3. הפעלת המודל (זה אוטומטית יפעיל את האנימציה מהתחלה!)
+        legoModel.SetActive(true);
+
+        // אופציונלי: מכבים את הסקריפט הזה כדי שלא יזמן את הלגו שוב ושוב בכל לחיצה
+        this.enabled = false;
+    }
+}
